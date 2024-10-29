23a8d91b9a46147c2a9b9c2783aed3fd30ec713f
lucasreali <lucasreali@github.com>
2024-10-29 08:47:09 -0300

    add server configuration

--- servidorDefin.py
+++ servidorDefin.py
@@ -1,2 +1,10 @@
 import socket 
 import threading
+
+HOST = "localhost"
+PORTA = 50001
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind((HOST, PORTA))
+servidor.listen()
+print("Servidor esperando conexão...")
+conexoes = {}
